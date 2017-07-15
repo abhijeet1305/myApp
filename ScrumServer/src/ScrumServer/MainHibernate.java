5c8d4ae2ebcbaa50f04b8217a52658468fa2c265
Soham Mahajan <ssoham.mahajan22@.com>
2017-07-15 22:04:22 +0530

    hbm failure changes

---
+++ ScrumServer/src/ScrumServer/MainHibernate.java
@@ -0,0 +1,39 @@
+package ScrumServer;
+
+import org.hibernate.Session;  
+import org.hibernate.SessionFactory;  
+import org.hibernate.Transaction;  
+import org.hibernate.cfg.Configuration;  
+
+import persistence.Users;
+  
+public class MainHibernate {  
+public static void main(String[] args) {  
+      
+    //creating configuration object  
+    Configuration cfg=new Configuration();  
+    cfg.configure("hibernate.cfg.xml");//populates the data of the configuration file  
+      
+    //creating seession factory object  
+    SessionFactory factory=cfg.buildSessionFactory();  
+      
+    //creating session object  
+    Session session=factory.openSession();  
+      
+    //creating transaction object  
+    Transaction t=session.beginTransaction();  
+          
+    Users user1=new Users();  
+    //user1.setId(115);  
+    user1.setUserName("Chinya");  
+    user1.setPassword("chinya");  
+      
+    session.persist(user1);//persisting the object  
+      
+    t.commit();//transaction is committed  
+    session.close();  
+      
+    System.out.println("successfully saved");  
+      
+}  
+}  
